1f18f81d0d16f17b340f981e0d8c4ce73f5a42bf
Daniel Momoh <daniel.momoh@stu.ocsb.ca>
2022-09-20 00:20:28 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/user/bin/env.python3
+
+#Created By: Daniel Momoh
+#Date: September 19 2022
+#This program displays Hello, world with proper style
+
+
+def main():
+    print("Hello, World with proper style")
+
+
+if __name__ == "__main__":
+    main()
